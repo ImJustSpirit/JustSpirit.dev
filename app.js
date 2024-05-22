60a4306198db8d6f5609a84e0b8399866e5df182
Jack Petty <77806303+ImJustSpirit@users.noreply.github.com>
2024-05-22 20:22:32 +0100

    Overhaul

--- app.js
+++ app.js
@@ -5,35 +5,3 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         splash.classList.add("display-none");
     }, 2000);
 })
-
-function folioAll() {
-    document.getElementById('JS.dev').style.display = "grid";
-    document.getElementById('Repacking').style.display = "grid";
-    document.getElementById('HighNoon').style.display = "grid";
-    document.getElementById('NCLCC').style.display = "grid";
-    document.getElementById('RLBCSA').style.display = "grid";
-}
-
-function folioProg() {
-    document.getElementById('JS.dev').style.display = "grid";
-    document.getElementById('Repacking').style.display = "grid";
-    document.getElementById('HighNoon').style.display = "none";
-    document.getElementById('NCLCC').style.display = "grid";
-    document.getElementById('RLBCSA').style.display = "grid";
-}
-
-function folioDes() {
-    document.getElementById('JS.dev').style.display = "none";
-    document.getElementById('Repacking').style.display = "grid";
-    document.getElementById('HighNoon').style.display = "grid";
-    document.getElementById('NCLCC').style.display = "none";
-    document.getElementById('RLBCSA').style.display = "none";
-}
-
-function folioProd() {
-    document.getElementById('JS.dev').style.display = "none";
-    document.getElementById('Repacking').style.display = "none";
-    document.getElementById('HighNoon').style.display = "grid";
-    document.getElementById('NCLCC').style.display = "grid";
-    document.getElementById('RLBCSA').style.display = "none";
-}
